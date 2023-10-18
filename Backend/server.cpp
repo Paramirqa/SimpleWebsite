5afb09752b24f6c4526606773ff151ef4412f463
Nydia Might <nydiamig@un-m3.kzn.21-school.ru>
2023-10-18 16:31:27 +0300

    Server v.1.2

--- Backend/server.cpp
+++ Backend/server.cpp
@@ -1,8 +1,8 @@
 #include "server.h"
 
+// Объявление функции для обработки клиентского запроса
 int main() {
-  int sockfd, client_socket;
-  struct sockaddr_in server_address, client_address;
+  int sockfd, client_socket = 0;
   socklen_t client_address_len = sizeof(client_address);
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,54 +27,82 @@ int main() {
     return 1;
   }
 
-  client_socket =
-      accept(sockfd, (struct sockaddr*)&client_address, &client_address_len);
-
-  if (client_socket < 0) {
-    std::cerr << "Error accepting connection" << std::endl;
-    return 1;
-  }
-
-  const int max_client_buffer_size = 1024;
-  char buf[max_client_buffer_size];
-  for (;;) {
-    ssize_t result = recv(client_socket, buf, max_client_buffer_size, 0);
-
-    std::stringstream response;
-    std::stringstream response_body;
-
-    if (result == -1) {
-      std::cerr << "Error receiving data" << std::endl;
-    } else if (result == 0) {
-      std::cerr << "Connection closed by client" << std::endl;
-    } else if (result > 0) {
-      buf[result] = '\0';
-
-      response_body << "<title>Test C++ HTTP Server</title>\n"
-                    << "<h1>Test page</h1>\n"
-                    << "<p>This is the body of the test page...</p>\n"
-                    << "<h2>Request headers</h2>\n"
-                    << "<pre>" << buf << "</pre>\n"
-                    << "<em><small>Test C++ HTTP Server</small></em>\n";
-
-      response << "HTTP/1.1 200 OK\r\n"
-               << "Version: HTTP/1.1\r\n"
-               << "Content-Type: text/html; charset=utf-8\r\n"
-               << "Content-Length: " << response_body.str().length()
-               << "\r\n\r\n"
-               << response_body.str();
-
-      ssize_t send_result = send(client_socket, response.str().c_str(),
-                                 response.str().length(), 0);
-
-      if (send_result == -1) {
-        std::cerr << "Error sending data" << std::endl;
-      }
-
-      close(client_socket);
+  while (true) {
+    client_socket =
+        accept(sockfd, (struct sockaddr*)&client_address, &client_address_len);
+    if (client_socket < 0) {
+      std::cerr << "Error accepting connection" << std::endl;
+      return 1;
     }
 
-    close(sockfd);
+    // Вызываем функцию для обработки клиентского запроса
+    ClientRequest(client_socket);
   }
+
+  close(sockfd);
   return 0;
+}
+
+// Определение функции для обработки клиентского запроса
+void ClientRequest(int client_socket) {
+  const int max_client_buffer_size = 1024;
+  char buf[max_client_buffer_size];
+
+  ssize_t bytesRead;
+  std::string request_body;  // Создаем переменную для хранения тела запроса
+
+  // Парсинг Боди-запроса
+  //   do {
+  //     bytesRead = recv(client_socket, buf, max_client_buffer_size, 0);
+  //     if (bytesRead > 0) {
+  //       request_body.append(buf, bytesRead);
+  //       bytesRead--;  // Добавляем данные к переменной request_body
+  //     }
+  //   } while (bytesRead > 0);
+
+  // Выводим тело запроса на стандартный вывод (для демонстрации)
+  std::cout << "HTTP Request Body:\n" << request_body << std::endl;
+
+  // Открываем файл для чтения
+  std::ifstream file(
+      "Test_files/aaaa.html");  // Замените "example.txt" на имя вашего файла
+
+  if (!file.is_open()) {
+    std::cerr << "Error opening file" << std::endl;
+    return;
+  }
+
+  std::stringstream response;
+  std::stringstream response_body;
+
+  while (true) {
+    // Читаем данные из файла в буфер
+    file.read(buf, max_client_buffer_size);
+    ssize_t bytesRead = file.gcount();
+
+    if (bytesRead == 0) {
+      break;  // Достигнут конец файла
+    }
+
+    response_body.write(buf, bytesRead);
+  }
+
+  file.close();  // Закрываем файл
+
+  // Остальной код для отправки данных клиенту остается без изменений
+  //   if ()
+  response << "HTTP/1.1 200 OK\r\n"
+           << "Version: HTTP/1.1\r\n"
+           << "Content-Type: text/html; charset=utf-8\r\n"
+           << "Content-Length: " << response_body.str().length() << "\r\n\r\n"
+           << response_body.str();
+
+  ssize_t send_result =
+      send(client_socket, response.str().c_str(), response.str().length(), 0);
+
+  if (send_result == -1) {
+    std::cerr << "Error sending data" << std::endl;
+  }
+
+  close(client_socket);
 }
--- Backend/server.h
+++ Backend/server.h
@@ -1,8 +1,13 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>  // Для close
+#include <unistd.h>
 
 #include <cstring>
+#include <fstream>
 #include <iostream>
 #include <sstream>
+
+struct sockaddr_in server_address, client_address;
+
+void ClientRequest(int client_socket);
